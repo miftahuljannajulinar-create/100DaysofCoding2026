36e207a5776e645b47439d0b0ed8384af38a108a
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-02 22:44:52 +0800

    Create Day1coding.java

---
+++ Day1coding.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // ini sebuah komentar satu baris
+        /* komentar lebih dari satu */
+        /** komentar dookumentasi **/
+
+        System.out.println("selamat malam");
+    }
+}

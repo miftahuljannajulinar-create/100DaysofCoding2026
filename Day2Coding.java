8662d1fbddc67c3f8f0272deb99a8fd859fd9cac
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-03 12:23:48 +0800

    Create Day2Coding.java

---
+++ Day2Coding.java
@@ -0,0 +1,16 @@
+public class Biodata {
+    public static void main(String[] args) {
+// Gunakan println()
+System.out.println("=== BIODATA DIRI SAYA ===");
+System.out.println(" Miftahul janna julinar ");
+
+// Gunakan print()
+System.out.print("Kelas saya:informatika A\n");
+System.out.print("Program studi:Informatika\n");
+System.out.print("Nim:D0226304\n");
+
+// Gunakan printf()
+System.out.printf("Universitas:%s%n","Universitas sulawesi barat");
+System.out.printf("Mahasiswa baru:%s%n","Fakultas teknik 2026");
+    }
+}

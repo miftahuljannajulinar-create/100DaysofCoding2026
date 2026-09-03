3c9cffd1920f0c23c4d4899852e74543a9d8988b
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-03 16:20:21 +0800

    Update Day2Coding.java

--- Day2Coding.java
+++ Day2Coding.java
@@ -1,15 +1,18 @@
 public class Biodata {
-    public static void main(String[] args) {
-// Gunakan println()
+   public static void main(String[] args) {
+// println() Digunakan sabagai untuk menampilkan sebuah teks dan otomatis pindah baris baru         
 System.out.println("=== BIODATA DIRI SAYA ===");
 System.out.println(" Miftahul janna julinar ");
 
-// Gunakan print()
+// print() Digunakan untuk menampilkan sebuah teks
+// \n ini Digunakan agar teks ini berpindah ke barisan berikut
 System.out.print("Kelas saya:informatika A\n");
 System.out.print("Program studi:Informatika\n");
 System.out.print("Nim:D0226304\n");
 
-// Gunakan printf()
+// printf() Digunakan untuk menapmpilakan teks dengan sebuah format tertentu
+// %s Digunakan untuk menampilkan data berupa sebuah teks
+// %n Digunakan untuk kita buat baris yang baru
 System.out.printf("Universitas:%s%n","Universitas sulawesi barat");
 System.out.printf("Mahasiswa baru:%s%n","Fakultas teknik 2026");
     }

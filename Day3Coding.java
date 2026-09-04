29b66d2b35afc524ac4448d6ccf134dcd9a4de64
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-04 08:51:37 +0800

    Create Day3Coding.java

---
+++ Day3Coding.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args)  {
+        // /b=Backspace adalah menghapus atau menggeser mundur 1 karakter
+        System.out.println("ABC\bD"); 
+        // /t=Tab adalah memberikan jarak seperti tombol tab
+        System.out.println("Nama:\tItta");
+        // \n=New line adalah memindahkan ke baris yang baru
+        System.out.println("Semangat:\nguys");
+        // \f=Form feed adalah pergantian sebuah halaman 
+        System.out.println("pasti:\fbisa"); 
+        // \r=Carriage Retrun adalah mengembalikan ke awal baris
+        System.out.println("ABC\rD");
+        // \'=Singel Quote adalah digunakan sebagai tanda petik satu
+        System.out.println("kata:\'informatika bisa]\'");
+        // \"=Double Quote adalah digunakan sebagai tanda petik dua
+        System.out.println(" aku: \" pasti bisa\"");
+        // \\=Backslash adalah digunakan sebagai menampilkan tanda backslash
+        System.out.println("Folder: C\\ Users\\itta");
+
+    }
+}

7598ef0567a3571dd8c53005eca0686cd14a0979
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-05 08:43:53 +0800

    Create Day4Coding.java

---
+++ Day4Coding.java
@@ -0,0 +1,16 @@
+public class Bilanganbulat {
+    public static void main(String[] args) {
+// byte digunakan untuk menyimpan bilangan bulat kecil
+byte umur = 18;
+System.out.println("Byte :" + umur);
+// short digunakan untuk menyimpan bilangan bulat
+short tahun = 2008;
+System.out.println("Short :" + tahun);
+// int digunakan untuk menyimpan bilangan bulat yang umum digunakan
+int jumlah = 583729;
+System.out.println("int :" + jumlah);
+// long digunakan untuk menyimpan bilangan bulat yang sangat besar
+long angkabesar = 9876543210L;
+System.out.println("Long :" + angkabesar);
+    }
+}

2f4e3bcc41e334a09b2df89155c90cf0e29a716d
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-06 14:03:21 +0800

    Create Day5Coding.java

---
+++ Day5Coding.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args)  {
+        // double dipakai untuk menyimpan sebuah angka yang bernilai pecahan
+        double nilai = 86.75;
+        System.out.println("double (nilai) = " + nilai);
+        // float dipakai untuk menyimpan  sebuah angka pecahan,tapi ketelitiannya lebih rendah
+        // huruf f dipakai untuk menandakan angka tersebut bertipe float
+        float tinggi = 164.5f;
+        System.out.println("float (tinggi) = " + tinggi);
+    }
+}

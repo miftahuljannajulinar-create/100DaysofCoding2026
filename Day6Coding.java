283627fc8c4b4d988c6ddcef65c250aa4233f10e
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-07 18:11:48 +0800

    Create Day6Coding.java

---
+++ Day6Coding.java
@@ -0,0 +1,15 @@
+public class DataKarakter {
+    public static void main(String[] args) {
+// char untuk menyimpan satu karakter 
+char huruf = 'I';
+System.out.println("huruf: " + huruf);
+// boolean digunakan untuk menyimpan sebuah kondisi yang memiliki dua nilai
+// true artinya kondisi benar terjadi
+boolean hadir = true;
+System.out.println("hadir: " + hadir);
+// false artinya kondisi yang tidak benar terjadi
+boolean izin = false;
+System.out.println("izin: " + izin);
+
+    }
+}

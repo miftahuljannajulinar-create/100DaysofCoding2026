ca7d9ef086da052c33d4bebe58fe0115236ff516
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-08 13:34:04 +0800

    Create Day7coding.java

---
+++ Day7coding.java
@@ -0,0 +1,16 @@
+public class StringData {
+    public static void main(String[] args)  {
+
+    //String ini kita gunakan untuk menyimpan sebuah data berupa teks
+    String Universitas = "Universitas sulawesi barat";
+    String Fakultas = "Fakultas teknik";
+    String Programstudi = "Informatika";
+    
+    //Menampilkan sebuah hasil data ke layar
+    System.out.println("Universitas : " + Universitas);
+    System.out.println("Fakultas : " + Fakultas);
+    System.out.println("Program studi : " + Programstudi);
+
+    
+    }
+}

663ddcaa8f9b6b0867ecc1a69a2f285958ed3840
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-09 19:43:04 +0800

    Create Day8Coding.java

---
+++ Day8Coding.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args)  {
+        //Deklarasi variabel 
+        //Membuat variabel umur tanpa memberikan nilai
+        int umur;
+
+        //Inisialisasi variabel 
+        //Memberikan nilai awal 17 pada variabel umur
+        umur = 17;
+        //Menampilkan hasil nilai awal variabel
+        System.out.println("Umur awal: " + umur);
+
+        //Update variabel 
+        //Mengubah nilai umur dari 17 jadi 18
+        umur = 18;
+        //Menampilkan hasil nilai satelah di update
+        System.out.println("Umur stelah di update: " + umur);
+    
+    }
+}

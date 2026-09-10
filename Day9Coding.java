89c97e294f346d0ef1d68aeba6c3c08dc020bad2
miftahuljannajulinar-create <miftahuljannajulinar@gmail.com>
2026-09-10 14:25:58 +0800

    Create Day9Coding.java

---
+++ Day9Coding.java
@@ -0,0 +1,9 @@
+public class Finalvariabel {
+    public static void main(String[] args)  {
+ //Membuat variabel final sebagai konstanta
+ //Nilai konstanta tidak dapat diubah setelah diberikan nilai
+ final int Jumlah_Mahasiswa = 30;
+ //Menampilkan nilai jumlah mahasiswa ke layar
+ System.out.println("Jumlah mahasiswa: " + Jumlah_Mahasiswa);      
+    }
+}
